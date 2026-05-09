f45a59600aace7c0485b0be6237d9ef840c52371
nzhulva <santosogulojowo03@gmail.com>
2026-05-09 12:59:57 +0700

    edit font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,14 +7,23 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Font Thunder Demo (self-hosted) -->
+        <style>
+            @font-face {
+                font-family: 'Thunder';
+                src: url('{{ asset("fonts/Thunder-BoldLC.woff2") }}') format('woff2');
+                font-weight: normal;
+                font-style: normal;
+            }
+            body {
+                font-family: 'Thunder', sans-serif;
+            }
+        </style>
     </head>
-    <body class="font-sans antialiased">
+    <body class="antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
