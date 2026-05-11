d52da2606bbddd5d173a4d998b3e83de6325947e
mrn0sleep <teofilusaditya194@gmail.com>
2026-05-10 20:59:31 -0400

    add lang id translations

---
+++ lang/id/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Password kamu berhasil direset.',
+    'sent' => 'Link reset password telah dikirim ke email kamu.',
+    'throttled' => 'Mohon tunggu sebentar sebelum mencoba lagi.',
+    'token' => 'Token reset password tidak valid atau sudah kadaluarsa.',
+    'user' => 'Kami tidak menemukan akun dengan alamat email tersebut.',
+];
